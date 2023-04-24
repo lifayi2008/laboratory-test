efc0d2f43c4cf1bf66b5aae35ecfb846bf207c48
LiFayi <lifayi2008@gmail.com>
2023-04-24 10:25:40 +0800

    bug fix

--- index.js
+++ index.js
@@ -30,110 +30,110 @@ async function request (code, chapter, reqData) {
 
 async function startAllExperiment() {
   //3.5
-  // console.log(`=======================3.5 start===================`)
-  // await request(0, '3.5');
-  // await sleep(10000);
-  // for(let i = 5; i >= 0; i--) {
-  //   let resistValue = '0L';
-  //   if(i === 5) {
-  //     resistValue = '4769';
-  //   }
-  //   let props = sampleData.map((item) => ({no: item.no, resistValue}))
-  //   let reqData = {...processCommon, leftTime: i.toString(), props}
-  //   await request(2, '3.5', reqData);
-  // }
-  // await sleep(10000);
-  // await request(1, '3.5');
-  // await sleep(10000);
-  // console.log(`=======================3.5 end===================`)
-  //
-  // console.log(`=======================3.6 start===================`)
-  // await request(0, '3.6');
-  // await sleep(10000);
-  // for(let i = 60; i > 0; i--) {
-  //   let props = sampleData.map((item) => ({no: item.no, standValtage: i + 3000 + '', leakCurrValue: '0.01'}))
-  //   let reqData = {...processCommon, leftTime: i.toString(), props}
-  //   await request(2, '3.6', reqData);
-  // }
-  // await sleep(10000);
-  // await request(1, '3.6');
-  // await sleep(10000);
-  // console.log(`=======================3.6 end===================`)
+  console.log(`=======================3.5 start===================`)
+  await request(0, '3.5');
+  await sleep(10000);
+  for(let i = 5; i >= 0; i--) {
+    let resistValue = '0L';
+    if(i === 5) {
+      resistValue = '4769';
+    }
+    let props = sampleData.map((item) => ({no: item.no, resistValue}))
+    let reqData = {...processCommon, leftTime: i.toString(), props}
+    await request(2, '3.5', reqData);
+  }
+  await sleep(10000);
+  await request(1, '3.5');
+  await sleep(10000);
+  console.log(`=======================3.5 end===================`)
 
-  // console.log(`=======================3.7 start===================`)
-  // await request(0, '3.7');
-  // await sleep(10000);
-  // await request(2, '3.7');
-  // await sleep(10000);
-  // await request(1, '3.7');
-  // await sleep(10000);
+  console.log(`=======================3.6 start===================`)
+  await request(0, '3.6');
+  await sleep(10000);
+  for(let i = 60; i > 0; i--) {
+    let props = sampleData.map((item) => ({no: item.no, standValtage: i + 3000 + '', leakCurrValue: '0.01'}))
+    let reqData = {...processCommon, leftTime: i.toString(), props}
+    await request(2, '3.6', reqData);
+  }
+  await sleep(10000);
+  await request(1, '3.6');
+  await sleep(10000);
+  console.log(`=======================3.6 end===================`)
 
-  // await request(2, '3.8');
-  // await sleep(10000);
-  // await request(1, '3.8');
-  // await sleep(10000);
-  // console.log(`=======================3.8 end===================`)
+  console.log(`=======================3.7 start===================`)
+  await request(0, '3.7');
+  await sleep(10000);
+  await request(2, '3.7');
+  await sleep(10000);
+  await request(1, '3.7');
+  await sleep(10000);
 
-  // console.log(`=======================3.9 start===================`)
-  // await request(0, '3.9');
-  // await sleep(10000);
-  // for(let i = 6; i > 0; i--) {
-  //   await request(2, '3.9');
-  // }
-  // await sleep(10000);
-  // await request(1, '3.9');
-  // await sleep(10000);
-  // console.log(`=======================3.9 end===================`)
-  //
-  // console.log(`=======================3.10 start===================`)
-  // await request(0, '3.10');
-  // await sleep(10000);
-  // await request(2, '3.10');
-  // await request(2, '3.10');
-  // await sleep(10000);
-  // await request(1, '3.10');
-  // console.log(`=======================3.10 end===================`)
-  //
-  // console.log(`=======================3.13 start===================`)
-  // await request(0, '3.13');
-  // await sleep(10000);
-  // for(let i = 9; i > 0; i--) {
-  //   await request(2, '3.13');
-  // }
-  // await sleep(10000);
-  // await request(1, '3.13');
-  // console.log(`=======================3.13 end===================`)
-  //
-  // console.log(`=======================3.12 start===================`)
-  // await request(0, '3.12');
-  // await sleep(10000);
-  // for(let i = 9; i > 0; i--) {
-  //   await request(2, '3.12');
-  // }
-  // await sleep(10000);
-  // await request(1, '3.12');
-  // await sleep(10000);
-  // console.log(`=======================3.12 end===================`)
+  await request(2, '3.8');
+  await sleep(10000);
+  await request(1, '3.8');
+  await sleep(10000);
+  console.log(`=======================3.8 end===================`)
 
-  // console.log(`=======================3.15 start===================`)
-  // await request(0, '3.15');
-  // await sleep(10000);
-  // for(let i = 9; i > 0; i--) {
-  //   await request(2, '3.15');
-  // }
-  // await sleep(10000);
-  // await request(1, '3.15');
-  // console.log(`=======================3.15 end===================`)
+  console.log(`=======================3.9 start===================`)
+  await request(0, '3.9');
+  await sleep(10000);
+  for(let i = 6; i > 0; i--) {
+    await request(2, '3.9');
+  }
+  await sleep(10000);
+  await request(1, '3.9');
+  await sleep(10000);
+  console.log(`=======================3.9 end===================`)
 
-  // console.log(`=======================3.14 start===================`)
-  // await request(0, '3.14');
-  // await sleep(10000);
-  // for(let i = 60; i > 0; i--) {
-  //   await request(2, '3.14');
-  // }
-  // await sleep(10000);
-  // await request(1, '3.14');
-  // console.log(`=======================3.14 end===================`)
+  console.log(`=======================3.10 start===================`)
+  await request(0, '3.10');
+  await sleep(10000);
+  await request(2, '3.10');
+  await request(2, '3.10');
+  await sleep(10000);
+  await request(1, '3.10');
+  console.log(`=======================3.10 end===================`)
+
+  console.log(`=======================3.13 start===================`)
+  await request(0, '3.13');
+  await sleep(10000);
+  for(let i = 9; i > 0; i--) {
+    await request(2, '3.13');
+  }
+  await sleep(10000);
+  await request(1, '3.13');
+  console.log(`=======================3.13 end===================`)
+
+  console.log(`=======================3.12 start===================`)
+  await request(0, '3.12');
+  await sleep(10000);
+  for(let i = 9; i > 0; i--) {
+    await request(2, '3.12');
+  }
+  await sleep(10000);
+  await request(1, '3.12');
+  await sleep(10000);
+  console.log(`=======================3.12 end===================`)
+
+  console.log(`=======================3.15 start===================`)
+  await request(0, '3.15');
+  await sleep(10000);
+  for(let i = 9; i > 0; i--) {
+    await request(2, '3.15');
+  }
+  await sleep(10000);
+  await request(1, '3.15');
+  console.log(`=======================3.15 end===================`)
+
+  console.log(`=======================3.14 start===================`)
+  await request(0, '3.14');
+  await sleep(10000);
+  for(let i = 60; i > 0; i--) {
+    await request(2, '3.14');
+  }
+  await sleep(10000);
+  await request(1, '3.14');
+  console.log(`=======================3.14 end===================`)
 }
 
 startAllExperiment().then(console.log);
